d04414ed61255b83a872cca3e6dbcfbdfcfc9a75
rebellehr <rileylehr@gmail.com>
2024-03-29 19:18:31 +0800

    Auto commit on logout

--- auto_git_backup.sh
+++ auto_git_backup.sh
@@ -2,10 +2,10 @@
 
 function auto_git_backup() {
     echo "Auto commit on logout"
-    sleep 20
-    git -C "$HOME"/shell add . >> $HOME/shell/log
-    git -C "$HOME"/shell commit -m "Auto commit on logout" >> $HOME/shell/log
-    git -C "$HOME"/shell push origin main >> $HOME/shell/log
+    sleep 5
+    git -C "$HOME"/shell add .
+    git -C "$HOME"/shell commit -m "Auto commit on logout"
+    git -C "$HOME"/shell push origin main
 }
 
 auto_git_backup
