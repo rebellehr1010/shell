48ce44915018cbad433260480b29634d2eedcebb
Riley Lehr <riley.lehr@connectsource.com.au>
2024-04-03 08:34:53 +0800

    Added files from local

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-alias resource="source ~/.bashrc"
+# Define the aliases
+aliases=(
+    "cubemx=/usr/local/STMicroelectronics/STM32Cube/STM32CubeMX/STM32CubeMX"
+    "cubeprogrammer=/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32CubeProgrammer"
+    "cubeprogrammercli=/usr/local/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI"
+    "sudo=sudo "
+    "mv=mv -i "
+    "cp=cp -i "
+    "rm=rm -i "
+    "find=find / -mount "
+    "resource=source ~/.bashrc"
+)
+echo ""
+echo "Creating Aliases"
+# Loop through each alias
+for alias_cmd in "${aliases[@]}"; do
+    # Split the alias and its command
+    IFS="=" read -r alias_name alias_command <<< "$alias_cmd"
+
+    # Check if alias already exists
+    existing_alias=$(alias "$alias_name" 2>/dev/null)
+    if [[ -z "$existing_alias" ]]; then
+        # Create the alias
+        alias "$alias_name"="$alias_command"
+        echo "    * Alias created: $alias_name -> $alias_command"
+    fi
+done
--- path.sh
+++ path.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+#!/bin/bash
+directories=(
+    "/opt/st/stm32cubeide_1.14.0"
+    "$HOME/.local/bin"
+    "$HOME/.local/share/applications"
+    "$HOME/local"
+    "$HOME/local/bin"
+    "$HOME/local/shell"
+    "$HOME/shell"
+)
+
+echo "Adding directories to PATH"
+# Loop through each directory
+for dir in "${directories[@]}"; do
+    # Check if directory is already in PATH
+    if [[ ":$PATH:" != *":$dir:"* ]]; then
+        # Directory is not in PATH, so add it
+        export PATH="$dir:$PATH"
+        echo "    * Added $dir to PATH"
+    fi
+done
+
+
 # Function to add paths to the PATH variable
 add_to_path() {
     local path
